fa1ee79fe177eecf08453b3af7a1777da79150e1
lazar2013 <l_georgiev1@mail.harpercollege.edu>
2026-01-21 01:30:07 -0600

    Add files via upload

---
+++ Week 2/assignment4.py
@@ -0,0 +1,6 @@
+x = float(input("Enter first number with decimal"))
+y = float(input("Enter second number with decimal"))
+
+z = (x * y)
+
+print("The product of the two decimals is", z)
---
+++ Week 2/assignment5.py
@@ -0,0 +1,11 @@
+string = ("cat")
+print("Words or letters with no numerical values such as:", string)
+print(type(string))
+
+float_number = float("2.99")
+print("Numbers with decimals such as:", float_number)
+print(type(float_number))
+
+integer_number = int("25")
+print("Whole numbers such as:", integer_number)
+print(type(integer_number))
